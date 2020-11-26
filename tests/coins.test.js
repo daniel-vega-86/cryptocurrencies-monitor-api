ef904b81b4385b58c47499fbee623920e7d74726
Daniel Vega <deveh91@gmail.com>
2020-11-26 14:53:44 -0500

    set tests for endpoint /cryptocoins

---
+++ tests/coins.test.js
@@ -0,0 +1,39 @@
+/**
+ * @jest-environment node
+ */
+
+const request = require("supertest");
+
+const app = require("../src/app");
+const User = require("../src/models/user");
+const { createUser } = require("./factory/user-factory");
+const { codes, messages } = require("../config/dictionary");
+
+let user;
+
+beforeEach(async () => {
+  await User.destroy({ truncate: true });
+  user = await createUser();
+});
+
+describe("GET Cryptocoins", () => {
+  test("Should no get coins for unauthorized user", async (done) => {
+    const response = await request(app).get("/cryptocoins").send();
+    expect(response.status).toBe(codes.unauthorized);
+    expect(response.text).toContain(messages.unauthorized);
+    done();
+  });
+
+  test("Should get coins for unauthorized user", async (done) => {
+    const { body } = await request(app).post("/users/login").send({
+      username: user.dataValues.username,
+      password: "contrasena",
+    });
+    const response = await request(app)
+      .get("/cryptocoins")
+      .set("Authorization", body.token)
+      .send();
+    expect(response.status).toBe(codes.ok);
+    done();
+  });
+});
