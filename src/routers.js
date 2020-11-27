e00ba6300b73867840bbd41cd16a7774ac53b0ee
Daniel Vega <deveh91@gmail.com>
2020-11-26 21:15:27 -0500

    implement /cryptocoins/assign endpoint for users

--- src/routers.js
+++ src/routers.js
@@ -8,7 +8,7 @@ const {
   closeSession,
   closeAllSessions,
 } = require("./controllers/user");
-const { getCoins } = require("./controllers/coins");
+const { getCoins, assignCoins } = require("./controllers/coins");
 const signUpSchema = require("./schemas/user-signup");
 
 const router = new express.Router();
@@ -17,7 +17,10 @@ router.post("/users", validatorBySchema(signUpSchema), createUser);
 router.post("/users/login", access);
 
 router.use(auth);
+
 router.get("/cryptocoins", getCoins);
+router.post("/cryptocoins/assign", assignCoins);
+
 router.post("/users/logout", closeSession);
 router.post("/users/logoutAll", closeAllSessions);
 
--- src/services/coins.js
+++ src/services/coins.js
@@ -1,4 +1,5 @@
 const api = require("../../config/axios");
+const Coin = require("../models/coins");
 
 const listCoins = async (preferedCurrency) => {
   return new Promise(async (resolve, reject) => {
@@ -6,17 +7,7 @@ const listCoins = async (preferedCurrency) => {
       const { data: coins } = await api.get("/coins/markets", {
         params: { vs_currency: preferedCurrency },
       });
-      const cryptoCoins = [];
-      coins.forEach((coin) => {
-        cryptoCoins.push({
-          id: coin.id,
-          symbol: coin.symbol,
-          price: coin.current_price,
-          name: coin.name,
-          image: coin.image,
-          lastUpdated: coin.last_updated,
-        });
-      });
+      const cryptoCoins = Coin.filterData(coins);
       resolve(cryptoCoins);
     } catch (e) {
       reject(e);
@@ -24,4 +15,31 @@ const listCoins = async (preferedCurrency) => {
   });
 };
 
-module.exports = { listCoins };
+const selectCoins = async (user, coinId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const assigned = await Coin.findOne({
+        where: { userId: user.id, coinId },
+      });
+      if (assigned) {
+        throw new Error("You had already assigned this cryptocurrency.");
+      }
+      const { data: coin } = await api.get("/coins/markets", {
+        params: { vs_currency: user.preferedCurrency, ids: coinId },
+      });
+      if (coin.length === 0) {
+        throw new Error("Cryptocurrencie not found.");
+      }
+      const cryptoCoin = Coin.filterData(coin);
+      await Coin.create({
+        userId: user.id,
+        coinId,
+      });
+      resolve(cryptoCoin);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+module.exports = { listCoins, selectCoins };
